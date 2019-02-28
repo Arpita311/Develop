dd18de81bd55f097738f012316c1e35e3647cecb
abhiraj45 <abhiraj.bais@click2cloud.net>
2019-02-28 10:38:24 +0530

    commit subtraction

--- demo.go
+++ demo.go
@@ -1,5 +1,7 @@
 package Demo
 
+import "fmt"
+
 func add() {
 
 	num1 := 10
@@ -7,10 +9,13 @@ func add() {
 	add := num1 + num2
 	println(add)
 }
-func sum() {
+func sumr() {
 
 	num1 := 10
 	num2 := 20
 	add := num1 + num2
 	println(add)
+}
+func sub() {
+	fmt.Println("You are in Subtraction.........")
 }
